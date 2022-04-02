2dd78733aa57070bd6a018df5b079f434a4eea72
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-02 12:16:23 -0400

    Add funion through a POST request.

--- app.js
+++ app.js
@@ -29,17 +29,35 @@ const articleSchema = {
 const Article = mongoose.model('Article', articleSchema);
 
 //query items inside of articles collection when client visists http://localhost:3000/articles
-app.get("/articles", (req, res) =>{
+app.get("/articles", (req, res) => {
   //query for all articles and log to console
-Article.find((err, foundArticles)=>{
-  if(!err){console.log(foundArticles);
+  Article.find((err, foundArticles) => {
+    if (!err) {
+      console.log(foundArticles);
+    } else {
+      console.log(err);
+    }
+
+  });
+});
+
+//when a post request is sent (using Postman currently)
+app.post("/articles", (req, res)=>{
+  //grab the title and content that was sent and create a new Article
+  const newArticle = new Article ({
+    title: req.body.title,
+    content: req.body.content
+  });
+  //then insert it into our collection
+newArticle.save((err)=>{
+  //tell the server if it was successful, or if it failed send the error.
+  if(!err){
+    res.send("Successfully inserted new article!");
   }else{
-    console.log(err);
+    res.send(err);
   }
-
 });
 });
-
 
 app.listen(port, function() {
 
